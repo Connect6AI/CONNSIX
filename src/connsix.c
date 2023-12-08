d9348b42cc3fa294428e7d3b1d68d9fe1fe3fd5b
joonmy <david516@naver.com>
2023-12-08 08:19:50 +0000

    blocking algorithm change

--- src/connsix.c
+++ src/connsix.c
@@ -482,6 +482,33 @@ void checkBlocked(position_t candidate[], int candidateindex, position_t newPosi
 		return;
 	}
 	else if (candidateindex == 2){
+		a = candidate[0];
+		b = candidate[1];
+		board[a.y][a.x] = player_color;
+		if(checkNo6(b)){
+			newPosition[0].x = a.x;
+			newPosition[0].y = a.y;
+			newPosition[1].x = -1;
+			newPosition[1].y = -1;
+
+			board[a.y][a.x] = EMPTY;
+			return;
+		}
+
+		board[b.y][b.x] = player_color;
+		if(checkNo6(a)){
+			newPosition[0].x = b.x;
+			newPosition[0].y = b.y;
+			newPosition[1].x = -1;
+			newPosition[1].y = -1;
+
+			board[b.y][b.x] = EMPTY;
+			return;
+		}
+			
+		board[a.y][a.x] = EMPTY;
+		board[b.y][b.x] = EMPTY;
+		
 		newPosition[0].x = candidate[0].x;
 		newPosition[0].y = candidate[0].y;
 		newPosition[1].x = candidate[1].x;
